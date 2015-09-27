e121b06ef5eacdf41b3f9afea153df384b6675ec
Arturo Mora <luartmgr@gmail.com>
2015-09-27 16:33:50 -0600

    ComponentFor genera tanto JSON como XML. Serializer tiene un nuevo método para generar JSON.

--- urSQL/src/urSQL/System/Serializer.java
+++ urSQL/src/urSQL/System/Serializer.java
@@ -10,8 +10,11 @@ import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.LinkedList;
+import com.google.gson.Gson;
 
 public class Serializer 
 {
@@ -27,6 +30,36 @@ public class Serializer
 		return this.rs;
 	}
 	
+	public static void main(String[] args) 
+	{
+		String tableName = "estudiante";
+		String nombre = "carne";
+		String tipo = "INTEGER";
+		String nombre2 = "nombre";
+		String tipo2 = "VARCHAR";
+		TableAttribute pk = new TableAttribute(nombre, tipo);
+		TableAttribute col1 = new TableAttribute(nombre, tipo);
+		TableAttribute col2 = new TableAttribute(nombre2, tipo2);
+		LinkedList<TableAttribute> columnas = new LinkedList<TableAttribute>();
+		columnas.add(col1);
+		columnas.add(col2);
+		LinkedList<String> reg1 = new LinkedList<String>();
+		reg1.add("2012");
+		reg1.add("hector");
+		LinkedList<String> reg2 = new LinkedList<String>();
+		reg2.add("2013");
+		reg2.add("holandes");
+		TableRegister treg1 = new TableRegister(reg1);
+		TableRegister treg2 = new TableRegister(reg2);
+		LinkedList<TableRegister> tdata = new LinkedList<TableRegister>();
+		tdata.add(treg1);
+		tdata.add(treg2);
+		TableData tableData = new TableData(tdata);
+		TableMetadata tMetaData = new TableMetadata(tableName, columnas, pk);
+		ResultSet bulldozer = new ResultSet(tableData, tMetaData);
+		Serializer culo = new Serializer(bulldozer);
+		culo.serializeJson();
+	}
 	public String serialize()
 	{
 		String tableName = this.rs.getTableMetadata().getTableName();
@@ -70,7 +103,7 @@ public class Serializer
 			StreamResult extremeResult = new StreamResult( outWriter );
 			transformer.transform( source, extremeResult); 
 			StringBuffer sb = outWriter.getBuffer(); 
-			return( sb.toString() );
+			return sb.toString();
 			
 		} 
 		catch (Exception e) 
@@ -78,5 +111,21 @@ public class Serializer
 			e.printStackTrace();
 			return ( "Error" );
 		}
+	}
+
+	public String serializeJson() 
+	{
+		Gson gson = new Gson();
+		String json = gson.toJson(this.rs);
+
+		try {
+			FileWriter writer = new FileWriter("table.json");
+			writer.write(json);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return json;
 	}
 }
